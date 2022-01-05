31e333616ab095caca6663dba84b356c0aa2c1da
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-05 19:28:05 +0000

    Project to send motivational quote every tuesday of the week.

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+import smtplib
+import datetime as dt
+
+now = dt.datetime.now()
+weekday = now.weekday()
+my_email = 'python.chinwe@gmail.com'
+pwd = 'pyt@#chinwe'
+
+if weekday == 2:
+    with open("quotes.txt") as quote_file:
+        quote_list = quote_file.readlines()
+        quote = random.choice(quote_list)
+        quote_only = quote[0:quote.find('-')]
+        quote_author = quote[quote.find('-'):len(quote) - 1]
+
+    with smtplib.SMTP("smtp.gmail.com", 587, timeout=30) as connection:
+        connection.starttls()
+        connection.login(my_email, pwd)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs='gnexplore@gmail.com',
+            msg=f"Subject:Motivational Quote"
+                f"\n\n{quote_only}\n\n{quote_author}"
+        )
+
